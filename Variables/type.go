9062b7954f4c8ffacbcc2a7af3281dfbcc29daf6
Arda Parlak <ardaparlak4@gmail.com>
2020-09-19 18:28:42 +0300

    type change

--- Variables/type.go
+++ Variables/type.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 )
 
-var y int = 25
+var a int
+type hotdog int // own type
+var b hotdog
 func main(){
-	fmt.Println(y)
-	fmt.Printf("%T\n", y)
+	a = 25
+	b=30
+
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
+	fmt.Printf("%T\n", b)
+	fmt.Println(b)
+	a = int (b) //This is a conversion not a casting
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
+
 	//y = "Arda" // because go lang is static type not dynamic type you dont change int to string or other type
 	//fmt.Println(y)
+
 }
 
