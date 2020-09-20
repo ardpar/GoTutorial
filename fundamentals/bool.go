e7c9be8f8d8c98e31747590fc4026370d5d4e352
Arda Parlak <ardaparlak4@gmail.com>
2020-09-20 18:08:29 +0300

    change

---
+++ fundamentals/bool.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//var x bool
+
+func main(){
+	a := 47
+	b := 30
+	fmt.Println(a == b)
+	fmt.Println(a < b)
+	fmt.Println(a > b)
+	fmt.Println(a >= b)
+	fmt.Println(a <= b)
+	fmt.Println(a != b)
+}
